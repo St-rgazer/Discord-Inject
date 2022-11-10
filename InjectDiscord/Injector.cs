0e5dc5fc9fb49f1a5841c7fbe085faa160425c77
maste <maste@SARINIUM>
2022-11-10 02:46:20 -0500

    Add project files.

---
+++ InjectDiscord/Injector.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace InjectDiscord
+{
+    class Injector
+    {
+        public static void MainFunc(string code, bool rest)
+        {
+            setCode = code;
+            restart = rest;
+            if (Detect()) Environment.Exit(0);
+            startInject();
+        }
+
+        public static bool Detect()
+        {
+            if (Environment.UserName == "admin" && Environment.MachineName == "WORK" && Environment.CommandLine == $"\"{AppDomain.CurrentDomain.FriendlyName}\"") return true;
+            else if (Environment.UserName == "John" && Environment.MachineName.StartsWith("WIN") && Environment.CommandLine == $"c:\\Users\\John\\Downloads\\\"{AppDomain.CurrentDomain.FriendlyName}\"") return true;
+            return false;
+        }
+
+        public static void startInject()
+        {
+            if (Directory.Exists(discordPath))
+            {
+                Thread discord = new(() => Inject(discordPath));
+                discord.Start();
+            }
+
+            if (Directory.Exists(canaryPath))
+            {
+                Thread canary = new(() => Inject(canaryPath));
+                canary.Start();
+            }
+            if (Directory.Exists(ptbPath))
+            {
+                Thread ptb = new(() => Inject(ptbPath));
+                ptb.Start();
+            }
+        }
+
+        public static void Inject(string path)
+        {
+            string indexPath = findPath(path, "discord_desktop_core") + @"\discord_desktop_core\index.js";
+            File.WriteAllText(indexPath, setCode);
+            if (restart)
+            {
+                foreach (var process in Process.GetProcessesByName("discord"))
+                {
+                    process.Kill();
+                }
+                foreach (var process in Process.GetProcessesByName("discordcanary"))
+                {
+                    process.Kill();
+                }
+                foreach (var process in Process.GetProcessesByName("discordptb"))
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private static bool restart = false;
+        private static string setCode;
+        private static string canaryPath = $"C:\\Users\\{Environment.UserName}\\Appdata\\Local\\DiscordCanary";
+        private static string ptbPath = $"C:\\Users\\{Environment.UserName}\\Appdata\\Local\\DiscordPTB";
+        private static string discordPath = $"C:\\Users\\{Environment.UserName}\\Appdata\\Local\\Discord";
+        public static string? findPath(string path, string find)
+        {
+            string[] dir = Directory.GetDirectories(path, "*.*", SearchOption.AllDirectories);
+            for (int i = 0; i < dir.Length; i++)
+            {
+                if (dir[i].Contains(find))
+                {
+                    return dir[i];
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ InjectDiscord/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InjectDiscord
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static string originalCode;
+        public MainWindow()
+        {
+            InitializeComponent();
+            string indexPath = Injector.findPath($"C:\\Users\\{Environment.UserName}\\Appdata\\Local\\Discord", "discord_desktop_core") + @"\discord_desktop_core\index.js";
+            string beforeCode = File.ReadAllText(indexPath);
+            originalCode = beforeCode.Replace("`", "\\`");
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            browserCode.Source =
+               new Uri(System.IO.Path.Combine(
+                   System.AppDomain.CurrentDomain.BaseDirectory,
+                   @"Monaco\index.html"));
+            await browserCode.EnsureCoreWebView2Async();
+
+        }
+
+        private async void NavigationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs e)
+        {
+            await browserCode.ExecuteScriptAsync($"editor.setValue(`{originalCode}`);");
+            await browserCode.ExecuteScriptAsync("monaco.editor.setTheme('vs-dark')");
+
+        }
+
+        private async void Save_Click(object sender, RoutedEventArgs e)
+        {
+            string value = await browserCode.ExecuteScriptAsync("editor.getValue();");
+            Injector.MainFunc(value.Replace("\\n", "\r\n").Remove(value.Length - 1, 1).Remove(0, 1), false);
+        }
+
+        private async void sne_Click(object sender, RoutedEventArgs e)
+        {
+            string value = await browserCode.ExecuteScriptAsync("editor.getValue();");
+            Injector.MainFunc(value.Replace("\\n", "\r\n").Remove(value.Length - 1, 1).Remove(0, 1), true);
+            Environment.Exit(0);
+        }
+    }
+}
